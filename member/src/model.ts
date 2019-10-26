ef24ce295276d30c2d44748a23cd5f4025a61cfe
Daniel <dani+github@daigeb.ch>
2019-10-26 22:40:53 +0200

    Membership modeling

--- member/src/model.ts
+++ member/src/model.ts
@@ -1,7 +1,7 @@
 export const names = {
   plural: 'members',
   singular: 'member'
-}
+};
 
 export interface Model {
   id: string;
@@ -9,14 +9,18 @@ export interface Model {
   lastName: number;
   email: string;
   address?: string;
-  zipCode?: number;
-  city?: number;
-  enlistment?: string;
-  gender?: 'female' | 'male' | 'unknown'
+  zipCode?: string;
+  city?: string;
+  membership?: {
+    from: string;
+    to?: string;
+  }[];
+  withdrawal?: string;
+  gender?: 'female' | 'male' | 'unknown';
   user: string;
   createdAt: string;
   updatedAt: string;
-};
+}
 
 export const schema = {
   "properties": {
@@ -25,11 +29,24 @@ export const schema = {
     "lastName": { "type": "string" },
     "email": { "type": "string", "format": "email" },
     "address": { "type": "string" },
-    "zipCode": { "type": "number" },
+    "zipCode": { "type": "string" },
     "city": { "type": "string" },
-    "enlistment": { "type": "string", "format": "date" },
+    "membership": {
+      "type": "array",
+      "items": {
+        "title": "Participant",
+        "description": "Participant connection schema",
+        "type": "object",
+        "properties": {
+          "from": { "type": "string", "format": "date" },
+          "to": { "type": "string", "format": "date" }                
+        },
+        "required": ["from"],
+        "additionalProperties": false
+      }
+    },
     "gender": { "type": "string", "enum": ["female", "male", "unknown"] },
   },
-  "required": ["firstName", "lastName"],
+  "required": ["firstName", "lastName", "membership"],
   "additionalProperties": false
 };
